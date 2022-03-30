4f34a0908780aea4ab0dac7ca4719c22bc9a52cc
Zühtü Ahmet SOLAK <zahmet@MacBook-Pro-von-Zuhtu.local>
2022-03-30 17:42:56 +0300

    30/03/2022

--- src/main/C01_main.java
+++ src/main/C01_main.java
@@ -1,7 +1,7 @@
-package MAin;
+package main;
 
 public class C01_main {
     public static void main(String[] args) {
-        System.out.println("MErhabaaahhdhsfkal");
+        System.out.println("Hello World");
     }
 }
